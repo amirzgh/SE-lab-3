d487ad59cd9af10e70d7c2874d0a104205102ed1
hessamasna <hessamasna@yahoo.com>
2023-08-02 13:59:32 +0330

    Add when and then

--- src/test/java/cucumber/MyStepdefs.java
+++ src/test/java/cucumber/MyStepdefs.java
@@ -1,8 +1,18 @@
 package cucumber;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class MyStepdefs {
     @Given("^Two input values and operator, (\\d+) and (\\d+) and ([*^\\/])$")
-    public void twoInputValuesAdd(int arg0, int arg1, String operator){}
+    public void twoInputValuesAdd(int arg0, int arg1, String arg2){}
+
+    @When("^I Calculate the Two values with operator$")
+    public void iAddTheTwoValues() {
+    }
+
+    @Then("^I expect the result (\\d+)$")
+    public void iExpectTheResult(int arg0) {
+    }
 }
