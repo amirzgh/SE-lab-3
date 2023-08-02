e37bc299f33cb9d67259350287a766a9d7e776f5
hessamasna <hessamasna@yahoo.com>
2023-08-02 15:59:13 +0330

    work on it

--- src/test/java/cucumber/MyStepdefs.java
+++ src/test/java/cucumber/MyStepdefs.java
@@ -1,18 +1,61 @@
 package cucumber;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 
 public class MyStepdefs {
-    @Given("^Two input values and operator, (\\d+) and (\\d+) and ([*^\\/])$")
-    public void twoInputValuesAdd(int arg0, int arg1, String arg2){}
+    private Main.Calculator calculator;
+    private int value1;
+    private int value2;
+    private String operator;
+    private int result;
 
-    @When("^I Calculate the Two values with operator$")
-    public void iAddTheTwoValues() {
+    @Given("^Two input values, (\\d+) and (\\d+)$")
+    public void twoInputValuesAnd(int arg0, int arg1) {
+        value1 = arg0;
+        value2 = arg1;
     }
 
-    @Then("^I expect the result (\\d+)$")
-    public void iExpectTheResult(int arg0) {
+    @When("^press the multiply button$")
+    public void pressTheMultiplyButton() {
+        result = calculator.multiply(value1, value2);
+        System.out.println(result);
     }
+
+    @When("^press the divide button$")
+    public void pressTheDivideButton() {
+        result = calculator.divide(value1, value2);
+        System.out.println(result);
+    }
+
+    @When("^press the power button$")
+    public void pressThePowerButton() {
+        result = calculator.power(value1, value2);
+        System.out.println(result);
+    }
+
+    @Then("^the result should be (\\d+) on the screen$")
+    public void theResultShouldBeOnTheScreen(int arg0) {
+        Assert.assertEquals(arg0, result);
+    }
+
+
+    @When("I press the {string} button")
+    public void pressTheOperatorButton(String operator) {
+        switch (operator) {
+            case "*":
+                calculator.multiply(value1, value2);
+                break;
+            case "/":
+                calculator.divide(value1, value2);
+                break;
+            case "^":
+                calculator.power(value1, value2);
+                break;
+        }
+    }
+
 }
--- src/test/resources/features/calculator.feature
+++ src/test/resources/features/calculator.feature
@@ -1,6 +1,32 @@
+@ParameterType("red|blue|yellow")  // regexp
+  public Color color(String color){  // type, name (from method)
+    return new Color(color);       // transformer function
+  }
 @tag
-  Feature: Calculator
-  Scenario: Calculate two numbers
-    Given Two input values and operator, 1 and 2 and 3
-    When I Calculate the Two values with operator
-    Then I expect the result 4
+Feature: Calculator
+
+  Scenario: Multiplication
+    Given Two input values, 1 and 2
+    When press the multiply button
+    Then the result should be 2 on the screen
+
+  Scenario: Division
+    Given Two input values, 1 and 2
+    When press the divide button
+    Then the result should be 2 on the screen
+
+  Scenario: Exponentiation
+    Given Two input values, 1 and 2
+    When press the power button
+    Then the result should be 2 on the screen
+
+  Scenario Outline: calculator
+    Given Two input values, <first> and <second>
+    When press the <operator> button
+    Then the result should be <result> on the screen
+    Examples:
+      | first | second | operator | result |
+      | 6     | 2      | *        | 12     |
+      | 6     | 2      | /        | 3      |
+      | 6     | 2      | ^        | 36     |
+
