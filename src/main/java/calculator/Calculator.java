a8a9f66e47d618078687668982768b41d0ac2bff
hessamasna <hessamasna@yahoo.com>
2023-08-03 17:15:41 +0330

    add methods

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,6 +2,26 @@ package calculator;
 
 public class Calculator {
     public int add(int a, int b) {
-        return a +b;
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) throws Exception {
+        if (b == 0) {
+            throw new Exception();
+        }
+        return a / b;
+    }
+
+    public int power(int a, int b) {
+        int result = 1;
+        while (b != 0) {
+            result *= a;
+            b--;
+        }
+        return result;
     }
 }
