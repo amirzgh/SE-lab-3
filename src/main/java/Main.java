5315e19b5d4f21ad2e0248b9db6e928dc18a8aa6
hessamasna <hessamasna@yahoo.com>
2023-08-04 13:55:32 +0330

    Get numbers from user

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to cip calculator");
+        System.out.println("please Enter your first number");
+        int number1 = scanner.nextInt();
+        System.out.println("please Enter your second number");
+        int number2 = scanner.nextInt();
+
+    }
+}
